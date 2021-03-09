e00456e9c0c7a65cfacbfd1bd3ccbb0e0cb6e390
Martijn van Erne <martijnvanerne@martijns-mbp.kpn>
2021-03-09 18:52:46 +0100

    remove cruft, create simple web-component

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Sample extends HTMLElement {
+  connectedCallback() {
+    const mountPoint = document.createElement('div');
+    this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    ReactDOM.render(<App/>, mountPoint);
+  }
+}
+
+customElements.define('sample-webcomponent', Sample);
